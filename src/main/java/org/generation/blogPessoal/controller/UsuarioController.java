85e58fcb2fbef94476945e9538663b82f024c929
Bruna <bruna41bergami@gmail.com>
2021-12-20 09:17:25 -0300

    @bruna41 [Adicionando metodo delete]

--- src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +44,11 @@ public class UsuarioController {
 	@PostMapping ("/cadastrar")
 	public ResponseEntity<Usuario> post (@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.cadastrarUsuario(usuario));
+	}
+	
+	@DeleteMapping ("/deleteuser")
+	public void delete (@PathVariable long id) {
+		repository.deleteAll();
 	}
 	
 }
